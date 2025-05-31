9dbdcb1ec3b1305ce77c8d25ca53fc725b62a429
ziugpro <nsnssnnsnssjsjskskan@gmail.com>
2025-05-31 21:40:01 +0700

    Update Youmomgay.lua

--- Youmomgay.lua
+++ Youmomgay.lua
@@ -7,12 +7,13 @@ local scripts = {
   --[[ Dead Rails ]]--
     [70876832253163]  = "https://raw.githubusercontent.com/ziugpro/DeadRails/refs/heads/main/DeadRails.lua",
     [116495829188952] = "https://raw.githubusercontent.com/ziugpro/DeadRails/refs/heads/main/DeadRails.lua",
-}
-local Game = {
---[[ Arise Crossover ]]--
+  --[[ Arise Crossover ]]--
     [7074860883] = "https://raw.githubusercontent.com/ziugpro/Free/refs/heads/main/AriseCrossover",
+  --[[ Blue Lock ]]--
+    [6325068386] = "https://raw.githubusercontent.com/ziugpro/Free/refs/heads/main/Blue-Lock",
+
 }
-local url = scripts[game.PlaceId] or Game[game.GameId]
+local url = scripts[game.PlaceId] or scripts[game.GameId]
 if url then
     loadstring(game:HttpGetAsync(url))()
 end

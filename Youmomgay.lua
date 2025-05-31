fd3d3e9507d7a63ddc07c319f4fa66afa23736a3
ziugpro <nsnssnnsnssjsjskskan@gmail.com>
2025-05-31 11:25:14 +0700

    Create Youmomgay.lua

---
+++ Youmomgay.lua
@@ -0,0 +1,8 @@
+--[ Stop Skiding 🤣🤣🖕🏼🖕🏼 ]]--
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ziugpro/Free/refs/heads/main/BloxFruit"))()
+elseif game.PlaceId == 70876832253163 or game.PlaceId == 116495829188952 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ziugpro/DeadRails/refs/heads/main/DeadRails.lua"))()
+else
+print("Your Mom Gay")
+end
